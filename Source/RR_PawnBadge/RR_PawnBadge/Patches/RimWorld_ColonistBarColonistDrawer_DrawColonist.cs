f01b5cb0046b6e1b2510b344dac60468b80df791
Rangel Reale <rangelspam@gmail.com>
2019-03-28 08:52:04 -0300

    Skill badges

--- Source/RR_PawnBadge/RR_PawnBadge/Patches/RimWorld_ColonistBarColonistDrawer_DrawColonist.cs
+++ Source/RR_PawnBadge/RR_PawnBadge/Patches/RimWorld_ColonistBarColonistDrawer_DrawColonist.cs
@@ -12,6 +12,9 @@ namespace RR_PawnBadge
     [HarmonyPatch(typeof(ColonistBarColonistDrawer), nameof(ColonistBarColonistDrawer.DrawColonist))]
     class RimWorld_ColonistBarColonistDrawer_DrawColonist
     {
+        const float ICON_WIDTH = 35f;
+        const float ICON_WIDTH_HALF = 35f / 2f;
+
         private static void Postfix(UnityEngine.Rect rect, Verse.Pawn colonist, Verse.Map pawnMap, bool highlight, bool reordering)
         {
             CompBadge cb = colonist.GetComp<CompBadge>();
@@ -19,13 +22,13 @@ namespace RR_PawnBadge
 
             if (cb.badges[0] != "")
             {
-                GUI.DrawTexture(new UnityEngine.Rect(rect.x - 15, rect.y - 15, 35, 35),
+                GUI.DrawTexture(new UnityEngine.Rect(rect.x - ICON_WIDTH_HALF, rect.y - ICON_WIDTH_HALF, ICON_WIDTH, ICON_WIDTH),
                     DefDatabase<BadgeDef>.GetNamed(cb.badges[0]).Symbol, ScaleMode.ScaleToFit);
             }
 
             if (cb.badges[1] != "")
             {
-                GUI.DrawTexture(new UnityEngine.Rect(rect.xMax - 15, rect.y - 15, 35, 35),
+                GUI.DrawTexture(new UnityEngine.Rect(rect.xMax - ICON_WIDTH_HALF, rect.y - ICON_WIDTH_HALF, ICON_WIDTH, ICON_WIDTH),
                     DefDatabase<BadgeDef>.GetNamed(cb.badges[1]).Symbol, ScaleMode.ScaleToFit);
             }
         }
