780553a308c6a06d55c3eef3fb3f8e13e8a98c26
Rangel Reale <rangelspam@gmail.com>
2019-03-29 14:39:19 -0300

    Settings

--- Source/RR_PawnBadge/RR_PawnBadge/Patches/RimWorld_ColonistBarColonistDrawer_DrawColonist.cs
+++ Source/RR_PawnBadge/RR_PawnBadge/Patches/RimWorld_ColonistBarColonistDrawer_DrawColonist.cs
@@ -13,22 +13,33 @@ namespace RR_PawnBadge
     class RimWorld_ColonistBarColonistDrawer_DrawColonist
     {
         const float ICON_WIDTH = 35f;
-        const float ICON_WIDTH_HALF = 35f / 2f;
 
         private static void Postfix(UnityEngine.Rect rect, Verse.Pawn colonist, Verse.Map pawnMap, bool highlight, bool reordering)
         {
             CompBadge cb = colonist.GetComp<CompBadge>();
             if (cb == null) return;
 
+            float iwidth = ICON_WIDTH;
+            switch (Settings.badgeSize)
+            {
+            case Settings.BadgeSize.Small:
+                iwidth = iwidth - 10f;
+                break;
+            case Settings.BadgeSize.Large:
+                iwidth = iwidth + 10f;
+                break;
+            }
+            float iwidth_half = iwidth / 2f;
+
             if (cb.badges[0] != "")
             {
-                GUI.DrawTexture(new UnityEngine.Rect(rect.x - ICON_WIDTH_HALF, rect.y - ICON_WIDTH_HALF, ICON_WIDTH, ICON_WIDTH),
+                GUI.DrawTexture(new UnityEngine.Rect(rect.x - iwidth_half, rect.y - iwidth_half, iwidth, iwidth),
                     DefDatabase<BadgeDef>.GetNamed(cb.badges[0]).Symbol, ScaleMode.ScaleToFit);
             }
 
             if (cb.badges[1] != "")
             {
-                GUI.DrawTexture(new UnityEngine.Rect(rect.xMax - ICON_WIDTH_HALF, rect.y - ICON_WIDTH_HALF, ICON_WIDTH, ICON_WIDTH),
+                GUI.DrawTexture(new UnityEngine.Rect(rect.xMax - iwidth_half, rect.y - iwidth_half, iwidth, iwidth),
                     DefDatabase<BadgeDef>.GetNamed(cb.badges[1]).Symbol, ScaleMode.ScaleToFit);
             }
         }
